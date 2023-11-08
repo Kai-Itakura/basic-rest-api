445fff428c8c163f7c2cba80d39bbc3beb3caa12
Kai Itakura <itakai199969@gmail.com>
2023-11-08 23:51:01 +0900

    create database and APIs

--- app/app.js
+++ app/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const sqlite3 = require('sqlite3')
+
 const dbPath = 'app/db/database.sqlite3'
 
-// Get all users
+app.use(express.static(path.join(__dirname, 'public')))
+
+/**
+ * Get all users
+ */
 app.get('/api/v1/users', (req, res) => {
-  // Connect database
+  // connect database
   const db = new sqlite3.Database(dbPath)
 
   db.all('SELECT * FROM users', (err, rows) => {
@@ -15,32 +21,37 @@ app.get('/api/v1/users', (req, res) => {
   db.close()
 })
 
-// Get a user
+/**
+ * Get a user
+ */
 app.get('/api/v1/users/:id', (req, res) => {
-  // Connect database
+  // connect database
   const db = new sqlite3.Database(dbPath)
   const id = req.params.id
 
-  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+  db.get(`SELECT * FROM users WHERE id=${id}`, (err, row) => {
     res.json(row)
-
-    db.close()
   })
+
+  db.close()
 })
 
-// Search users matching keyword
+/**
+ * Search users match keywords
+ */
 app.get('/api/v1/search', (req, res) => {
   // connect database
   const db = new sqlite3.Database(dbPath)
+
   const keyword = req.query.q
 
-  db.all(`SELECT * FROM users WHERE name LIKE '%${keyword}%'`, (err, rows) => {
+  db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
     res.json(rows)
-
-    db.close()
   })
+
+  db.close()
 })
 
-const port = process.env.PORT || 3000
-app.listen(port)
-console.log(`Listen on port: ${port}`)
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
+console.log(`Listen on port ${PORT}!`)
